d03b0c4c386eceb6fe0eda10beb8a81d65248a2d
Lorenzo <baccini.lorenzo@gmail.com>
2021-09-09 16:34:02 +0200

    Esercizio finito senza bonus

---
+++ js/main.js
@@ -0,0 +1,25 @@
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: "#root",
+    data: {
+        newToDo: "",
+        toDos: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato"
+        ]
+     
+    },
+    methods: {
+        addToDo() {
+            if(this.newToDo != ""){
+                this.toDos.push(this.newToDo);
+                this.newToDo = "";
+            }
+        },
+        removeToDo(index) {
+            this.toDos.splice(index, 1);
+        }
+    }
+});
